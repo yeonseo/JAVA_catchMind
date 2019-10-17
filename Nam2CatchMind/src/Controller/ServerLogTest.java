9f675bfab029df12733a1f9c7730d70ba948cd0f
YanSoCoder <yulga_nara@naver.com>
2019-10-17 13:41:08 +0900

    AlertDisplay 클래스로 모듈화, 로그인 성공시 스레드 생성 테스트 시작

---
+++ Nam2CatchMind/src/Controller/ServerLogTest.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ServerLogTest implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+
+	}
+
+}
