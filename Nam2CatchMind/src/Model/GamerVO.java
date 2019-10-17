b793060a0d1a9aaf10251ef8653d584b1ffc66f2
chaehyeon01 <52403199+chaehyeon01@users.noreply.github.com>
2019-10-17 13:57:59 +0900

    첫백업 (게임메인,gameDAO,gameVO,loginController,membershipController,)

---
+++ Nam2CatchMind/src/Model/GamerVO.java
@@ -0,0 +1,58 @@
+package Model;
+
+public class GamerVO {
+	private String UserID;
+	private String UserPassword;
+	private String UserGender;
+	private String Image;
+	/* 
+	 * 
+	 * gameUser를 등록하기위한것
+	 * 
+	 * 
+	 */
+	
+	public GamerVO(String userID, String userPassword, String userGender, String image) {
+		super();
+		UserID = userID;
+		UserPassword = userPassword;
+		UserGender = userGender;
+		Image = image;
+	}
+
+	public GamerVO() {
+		super();
+	}
+	public String getUserID() {
+		return UserID;
+	}
+
+	public void setUserID(String userID) {
+		UserID = userID;
+	}
+
+	public String getUserPassword() {
+		return UserPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		UserPassword = userPassword;
+	}
+
+	public String getUserGender() {
+		return UserGender;
+	}
+
+	public void setUserGender(String userGender) {
+		UserGender = userGender;
+	}
+
+	public String getImage() {
+		return Image;
+	}
+
+	public void setImage(String image) {
+		Image = image;
+	}
+
+}
