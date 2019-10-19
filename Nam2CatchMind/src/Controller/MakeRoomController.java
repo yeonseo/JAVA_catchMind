0feb96d5d2a1d152af00d892690a46cf139d6a85
YanSoCoder <yulga_nara@naver.com>
2019-10-19 13:43:17 +0900

    Update MakeRoomController.java

--- Nam2CatchMind/src/Controller/MakeRoomController.java
+++ Nam2CatchMind/src/Controller/MakeRoomController.java
@@ -3,6 +3,7 @@ package Controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -17,7 +18,9 @@ public class MakeRoomController implements Initializable {
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		btnCancel.setOnAction(e->{
+			
+		});
 
 	}
 
